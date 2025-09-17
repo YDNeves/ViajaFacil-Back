f5acb63557191ac16e1e5a74d488eb43f0818347
Yoane Das Neves <yoanemarcelofneves@gmail.com>
2025-09-18 00:38:56 +0100

    a.1

--- src/routes/searchHistory.routes.ts
+++
@@ -1,9 +0,0 @@
-import { FastifyInstance } from "fastify";
-import { SearchHistoryController } from "../controllers/searchHistory.controller.js";
-import { authenticate } from "../lib/middlewares/authenticate.js";
-
-export async function searchHistoryRoutes(app: FastifyInstance) {
-  app.get("/users/:userId/search-history", { preHandler: [authenticate] }, SearchHistoryController.listByUser);
-  app.post("/search-history", { preHandler: [authenticate] }, SearchHistoryController.create);
-  app.delete("/users/:userId/search-history", { preHandler: [authenticate] }, SearchHistoryController.clear);
-}
