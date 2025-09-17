c90e2882e9e24bf5624a2b51c7210260a05d5c4a
Yoane Das Neves <yoanemarcelofneves@gmail.com>
2025-09-18 00:41:52 +0100

    a.1.1

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,6 @@ import { hotelRoutes } from "./hotel.routes";
 import { roomRoutes } from "./room.routes";
 import { reviewRoutes } from "./review.routes";
 import { bookingRoutes } from "./booking.routes";
-import { searchHistoryRoutes } from "./searchHistory.routes";
 import { userRoutes } from "./user.routes";
 import authRoutes from "./auth.routes";
 import { cityRoutes } from "./city.routes";
@@ -14,7 +13,6 @@ export async function registerRoutes(app: FastifyInstance) {
   await roomRoutes(app);
   await reviewRoutes(app);
   await bookingRoutes(app);
-  await searchHistoryRoutes(app);
   await userRoutes(app);
   await authRoutes(app);
 }
